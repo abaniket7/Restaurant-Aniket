c8ba7ea96a05145640970a8ce23b5da1bffa3004
Aniket Barua <131402530+abaniket7@users.noreply.github.com>
2023-11-20 04:00:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,35 @@
-$(document).ready(function () {
+$(document).ready(function(){
     // Add active class to the current link in the navigation
-    $('nav a').on('click', function () {
+    $('nav a').on('click', function(){
         $('nav a').removeClass('active');
         $(this).addClass('active');
     });
-
+});
+$(document).ready(function(){
     // Toggle responsive navigation menu
-    $('.navbar').on('click', function () {
+    $('.navbar').on('click', function(){
         $('nav').toggleClass('active');
     });
+});
 
-    function toggleLike(element) {
-        element.classList.toggle('liked');
-    }
-
+function toggleLike(element) {
+    element.classList.toggle('liked');
+}
+document.addEventListener('DOMContentLoaded', function () {
     // Selecting the elements
     let navbar = document.querySelector('.navbar');
     let menuBtn = document.querySelector('#menu-btn');
-    let loginForm = document.querySelector('.login-form-container');
 
     // Adding click event to menu button
     menuBtn.addEventListener('click', function () {
         navbar.classList.toggle('active');
     });
-
-    document.querySelector('#login-btn').onclick = () => {
-        loginForm.classList.toggle('active');
-        navbar.classList.remove('active');
-    };
 });
+
+let loginForm = document.querySelector('.login-form-container');
+let navbar = document.querySelector('.header .navbar');
+
+document.querySelector('#login-btn').onclick = () =>{
+    loginForm.classList.toggle('active');
+    navbar.classList.remove('active');
+}
