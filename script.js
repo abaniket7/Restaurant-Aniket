4576129b4288b405f08496cb4965a091da2ae521
Aniket Barua <131402530+abaniket7@users.noreply.github.com>
2023-12-06 14:24:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,48 @@
-$(document).ready(function(){
-    // Add active class to the current link in the navigation
-    $('nav a').on('click', function(){
-        $('nav a').removeClass('active');
-        $(this).addClass('active');
-    });
-});
-$(document).ready(function(){
-    // Toggle responsive navigation menu
-    $('.navbar').on('click', function(){
-        $('nav').toggleClass('active');
-    });
-});
-
-function toggleLike(element) {
-    element.classList.toggle('liked');
-}
+// Vanilla JavaScript for responsive navigation
 document.addEventListener('DOMContentLoaded', function () {
-    // Selecting the elements
     let navbar = document.querySelector('.navbar');
     let menuBtn = document.querySelector('#menu-btn');
 
-    // Adding click event to menu button
     menuBtn.addEventListener('click', function () {
         navbar.classList.toggle('active');
     });
 });
 
+// Handling login form visibility
 let loginForm = document.querySelector('.login-form-container');
 let navbar = document.querySelector('.header .navbar');
 
-document.querySelector('#login-btn').onclick = () =>{
+document.querySelector('#login-btn').onclick = () => {
     loginForm.classList.toggle('active');
     navbar.classList.remove('active');
+};
+
+document.addEventListener('mouseup', function (e) {
+    if (!loginForm.contains(e.target)) {
+        loginForm.classList.remove('active');
+    }
+});
+
+// JavaScript code to handle the "add to cart" click event
+function addToCartClicked() {
+    event.preventDefault();
+    alert('Item added to cart!');
 }
+
+// Function to toggle the like icon
+function toggleLike(element) {
+    element.classList.toggle('liked');
+}
+
+// JavaScript code to handle the form submission
+document.getElementById('orderForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    var name = document.querySelector('input[placeholder="Enter customer\'s name"]').value;
+    var number = document.querySelector('input[placeholder="Enter customer\'s number"]').value;
+
+    // Show a custom message
+    alert('Order placed successfully!\nName: ' + name + '\nNumber: ' + number);
+
+    event.target.reset();
+});
